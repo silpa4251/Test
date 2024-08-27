337f7ff981e321e107bbfc87c54064600db2fe0e
Silpa das.N <132433842+silpa4251@users.noreply.github.com>
2024-08-27 16:52:15 +0530

    Update Userefer.js

--- src/Components/Userefer.js
+++ src/Components/Userefer.js
@@ -1,23 +1,25 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import './Userefer.css'
 import Header from './Header';
 import Main from './Main';
 import Content from './Content';
 
 const Userefer = () => {
-    const buttons = [{id:1}, {id:2}, {id:3}];
-    // const [view,setView]  = useState();
-    const inputRef = useRef([]);
+    const buttons = [
+        {id:1 , component : <Header />}, 
+        {id:2 , component : <Content /> },
+        {id:3, component : <Main />}];
+            
+    const [view,setView]  = useState();
 
     const handleChange = (index) => {
-     inputRef.current[index]= index;
-        
+     setView(buttons[index].component);
     }
    
     return (
           <div>
-            {/* {view} */}
-            {buttons.map((btn)=><button  onClick={() => {handleChange(index)}} index={buttons.id}>{btn}</button>)}
+            {view}
+            {buttons.map((btn, index)=><button  onClick={() => {handleChange(index)}} key={btn.id}>Button {btn.id}</button>)}
            
           </div>
     )
